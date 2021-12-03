a77a744fff936871d457914f01a238821de81dfa
hyeonji0409 <guswl8507@naver.com>
2021-12-03 17:51:25 +0900

    front - css

---
+++ frontend_practice/src/main/java/com/Full_Stack_Study/frontend_practice/FrontendPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.Full_Stack_Study.frontend_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontendPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontendPracticeApplication.class, args);
+	}
+
+}
---
+++ frontend_practice/src/test/java/com/Full_Stack_Study/frontend_practice/FrontendPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Full_Stack_Study.frontend_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontendPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
